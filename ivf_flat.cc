6f4441ce707db858d64ececa0864dff17ba3fc3d
masa1u <88305358+masa1u@users.noreply.github.com>
2024-08-06 11:59:28 +0900

    fix ivf_flat recall

--- ivf_flat.cc
+++ ivf_flat.cc
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
   ivf_index.buildIndex(dataset);
   for (int ivf_flat_nprobe = lower_nprobe; ivf_flat_nprobe <= upper_nprobe; ivf_flat_nprobe += step_nprobe)
   {
-    for (int _ = 0; num_runs; num_runs++)
+    for (int _ = 0; _ < num_runs; _++)
     {
       bool start = false;
       bool quit = false;
@@ -86,59 +86,11 @@ int main(int argc, char *argv[])
       {
         throughput += IndexResults[i].queries_count;
       }
-      std::cout << "[IVF_FLAT]Throughput: " << throughput / ex_time << " [qps]" << std::endl;
-      std::cout << "[IVF_FLAT]Recall: " << calculateRecall(dataset) << std::endl;
+      std::pair<int, int> result = calculateRelevantAndRetrieved(dataset);
+      std::cout << "[IVF_FC]Throughput: " << throughput / ex_time << " [qps]" << result.second / ex_time << std::endl;
+      std::cout << "[IVF_FC]Recall: " << double(result.first) / result.second << std::endl;
 
       IndexResults.clear();
     }
   }
-  // ivf_index.printClusters();
-
-  // for (size_t i = 0; i < num_threads; ++i)
-  // {
-  //   ivf_flat_thv.emplace_back(ivf_flat_worker, i, std::ref(readys[i]), std::ref(start), std::ref(quit), &ivf_index, dimension, top_k, ivf_flat_nprobe);
-  // }
-
-  // while (true)
-  // {
-  //   bool failed = false;
-  //   for (auto &re : readys)
-  //   {
-  //     if (!__atomic_load_n(&re, __ATOMIC_SEQ_CST))
-  //     {
-  //       failed = true;
-  //       break;
-  //     }
-  //   }
-  //   if (!failed)
-  //   {
-  //     break;
-  //   }
-  // }
-
-  // __atomic_store_n(&start, true, __ATOMIC_SEQ_CST);
-  // std::this_thread::sleep_for(std::chrono::milliseconds(1000 * ex_time));
-
-  // __atomic_store_n(&quit, true, __ATOMIC_SEQ_CST);
-
-  // for (auto &th : ivf_flat_thv)
-  // {
-  //   th.join();
-  // }
-
-  // // 結果の出力
-  // int throughput = 0;
-  // for (int i = 0; i < num_threads; i++)
-  // {
-  //   throughput += IndexResults[i].queries_count;
-  // }
-  // std::cout << "[IVF_FLAT]Throughput: " << throughput / ex_time << " [qps]" << std::endl;
-  // std::cout << "[IVF_FLAT]Recall: " << calculateRecall(dataset) << std::endl;
-
-  // std::vector<std::thread> ivf_fc_thv;
-  // start = false;
-  // quit = false;
-  // readys = std::vector<int>(num_threads, 0);
-  // IndexResults.clear();
-  // IndexResults.resize(num_threads);
 }
